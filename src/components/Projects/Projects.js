6f9ccfb44f4603a519c3addbc26c3a381841f268
AbdelmajidBen <abdelmajid.benjelloun@etu.uae.ac.ma>
2024-06-15 00:39:34 +0100

    the final version

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,6 +8,11 @@ import editor from "../../Assets/Projects/codeEditor.png";
 import chatify from "../../Assets/Projects/chatify.png";
 import suicide from "../../Assets/Projects/suicide.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
+import chat from '../../Assets/Projects/Untitled_Project-removebg-preview.png'
+import pfm from '../../Assets/Projects/Untitled_Project__1_-removebg-preview.png'
+import twit from '../../Assets/Projects/Untitled_Project__2_-removebg-preview.png'
+import perc from '../../Assets/Projects/Untitled_Project__3_-removebg-preview.png'
+import coca from '../../Assets/Projects/Untitled_Project__4_-removebg-preview.png'
 
 function Projects() {
   return (
@@ -23,70 +28,59 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={chat}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="FSTT Chatbot"
+              description="Developing a chatbot fine-tuned on a custom context based on French language models: case of study FSTT. using llama3, Docker, LORA, Rag, FineTunning, Svelte."
+              ghLink="https://github.com/AbdelmajidBen/FSTT-LLM-Chat-Bot"
+
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={twit}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="Twitter Sentiment Analysis"
+              description="Develloping a web application that allows users to analyze the sentiment of tweets in real-time. The application uses the Twitter Data to fetch tweets and then uses a machine learning model to predict the sentiment of the tweets. The application is built using django, Kafka, and Docker."
+              ghLink="https://github.com/AbdelmajidBen/Twitter_Sentim_Analysis"
+
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={coca}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="Cocal Cola WebApp"
+              description="Developing a web application that allows the users and admins to facilitate the management of the company's products and orders. The application is built using MERNG stack (MongoDB, Express, React, Node, GraphQL) and Docker. with JWT authentication. Cloudinary for image storage."
+              ghLink="https://github.com/AbdelmajidBen/Cocal_Cola_WebApp"
+            
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={pfm}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="ML APP"
+              description="Developing a laptop application that allows users to upload datasets of their own or creating it while choosing the model they want to use for the prediction. The application is built using Python, Tkinter, and Scikit-learn."
+              ghLink="https://github.com/AbdelmajidBen/PFM_PA_Python"
+
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={perc}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+              title="Multi Layer Perceptron"
+              description="Developing the concept of the Multi layer perceptron in python with a custom dataset with visualization"
+              ghLink="https://github.com/AbdelmajidBen/Multi_layer_perceptron"
             />
           </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
-            />
-          </Col>
+
         </Row>
       </Container>
     </Container>
